fde32eeef28ccca089b487fd5bab97fcc15a7d1c
Danilo Kulisic <danilokulisic4@gmail.com>
2023-06-20 19:19:12 +0200

    adding styles to blog and add source button

--- component/Blog/index.tsx
+++ component/Blog/index.tsx
@@ -5,11 +5,11 @@ import { IBlogProps } from '@lib/types';
 
 export const Blog: FC<IBlogProps> = () => {
   return (
-    <section className="py-6 sm:py-12 dark:bg-gray-800 dark:text-gray-100 blog-section">
+    <section className="py-6 sm:py-12 bg-emerald dark:text-gray-100 blog-section">
     <div className="container p-6 mx-auto space-y-8">
       <div className="space-y-2 text-center">
-        <h2 className="text-3xl font-bold">Partem reprimique an pro</h2>
-        <p className="font-serif text-sm dark:text-gray-400">Qualisque erroribus usu at, duo te agam soluta mucius.</p>
+        <h2 className="text-3xl font-bold text-amber-950">Partem reprimique an pro</h2>
+        <p className="font-serif text-sm dark:text-amber-950">Qualisque erroribus usu at, duo te agam soluta mucius.</p>
       </div>
       <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
         <article className="flex flex-col dark:bg-gray-900">
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,9 +1,9 @@
 export const CATEGORIES: any = [
 	{ name: "Strength", color: "#c5b340"  },
 	{ name: "Endurance", color: "#c5b340" },
-	{ name: "Mind", color: "#c5b340" },
-	{ name: "Heart", color: "#c5b340" },
-	{ name: "Mushrooms", color: "#c5b340" },
+	{ name: "Mental_Health", color: "#c5b340" },
+	{ name: "Heart_Health", color: "#c5b340" },
+	{ name: "Mushroom_World", color: "#c5b340" },
 	{ name: "Workout", color: "#c5b340" },
 	{ name: "Science", color: "#c5b340" },
 	{ name: "News", color: "#c5b340" },
