20af5aa2409b8fdba0a91b98d915b55bb19130bf
Danilo Kulisic <danilokulisic4@gmail.com>
2023-06-24 19:41:19 +0200

    adding styles to the buttons

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,12 +1,12 @@
 export const CATEGORIES: any = [
-	{ name: "Strength", color: "#c5b340"  },
-	{ name: "Endurance", color: "#c5b340" },
-	{ name: "Mental_Health", color: "#c5b340" },
-	{ name: "Heart_Health", color: "#c5b340" },
-	{ name: "Mushroom_World", color: "#c5b340" },
-	{ name: "Workout", color: "#c5b340" },
-	{ name: "Science", color: "#c5b340" },
-	{ name: "News", color: "#c5b340" },
+	{ name: "Strength", color: "#14b8a6" },
+	{ name: "Endurance", color: "#14b8a6" },
+	{ name: "Mental_Health", color: "#14b8a6" },
+	{ name: "Heart_Health", color: "#14b8a6" },
+	{ name: "Mushroom_World", color: "#14b8a6" },
+	{ name: "Workout", color: "#14b8a6" },
+	{ name: "Science", color: "#14b8a6" },
+	{ name: "News", color: "#14b8a6" },
 ];
 
 console.log(CATEGORIES);
