e431ff378f3ea0ff5fde5144d915b949773608fb
slavo3dev <slavo@slavo.io>
2023-08-03 16:10:34 -0500

    cleaning: eslint formating the code

--- pages/api/weatherData.ts
+++ pages/api/weatherData.ts
@@ -1,21 +1,26 @@
-const apiKey = process.env.API_KEY;
-const location = 'Belgrade, Serbia';
+import type { NextApiRequest, NextApiResponse } from "next";
 
-const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`;
 
-export default function handler(req, res) {
-  fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Error fetching weather data');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((error) => {
-      console.error('Error fetching weather data:', error);
-      res.status(500).json({ error: 'Error fetching weather data' });
-    });
+export default function handler ( req: NextApiRequest, res: NextApiResponse )
+{
+    
+	const apiKey = process.env.API_KEY;
+	const location = "Belgrade, Serbia";
+	const url = `https://api.weatherapi.com/v1/current.json?key=${ apiKey }&q=${ location }`;
+    
+    
+	fetch(url)
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Error fetching weather data");
+			}
+			return response.json();
+		})
+		.then((data) => {
+			res.status(200).json(data);
+		})
+		.catch((error) => {
+			console.error("Error fetching weather data:", error);
+			res.status(500).json({ error: "Error fetching weather data" });
+		});
 }
