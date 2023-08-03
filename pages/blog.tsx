5cb865feb22ed5b2a3823ace77f68112687a67b8
Danilo Kulisic <danilokulisic4@gmail.com>
2023-08-03 22:59:55 +0200

    adding blog hygraphcms

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,13 +2,14 @@ import { FC } from "react";
 import { NavBar } from "component/Navbar";
 import { BlogWelcome } from "component/Blog";
 import { Layout } from "component/Layout";
-
+import { BlogPosts } from "component/Post";
 
 const BlogPage: FC = () => {
   return (
     <Layout>
-          <NavBar />
-          <BlogWelcome />
+      <NavBar />
+      <BlogWelcome />
+      <BlogPosts />
     </Layout>
   );
 };
