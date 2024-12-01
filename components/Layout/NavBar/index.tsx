4adc6708f63eb77fc8c2783380baeff7745e8f62
Slavo Popovic <slavo@slavo.io>
2024-12-01 11:57:27 -0500

    nav bar

---
+++ components/Layout/NavBar/index.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useState, useEffect } from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+
+export default function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+  return (
+    <Button
+      variant="link"
+      size="icon"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      {theme === "dark" ? <Sun size={18} /> : <Moon size={18} />}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
